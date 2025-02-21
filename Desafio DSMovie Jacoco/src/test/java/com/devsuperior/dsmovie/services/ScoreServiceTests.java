3b29fe7e36ef0280f472111f9c782c61c9ef18fc
FelipeGabrill <felipegabrielodss@gmail.com>
2025-02-21 16:02:59 -0300

    saveScoreShouldReturnMovieDTO, saveScoreShouldThrowResourceNotFoundExceptionWhenNonExistingMovieId

--- Desafio DSMovie Jacoco/src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ Desafio DSMovie Jacoco/src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,9 +1,30 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
@@ -11,11 +32,66 @@ public class ScoreServiceTests {
 	@InjectMocks
 	private ScoreService service;
 	
+	@Mock
+	private UserService userService;
+	
+	@Mock
+	private MovieRepository movieRepository;
+
+	@Mock 
+	private ScoreRepository scoreRepository;
+	
+	private long existingMovieId, nonExistingMovieId;
+		
+	private ScoreEntity score;
+	private ScoreDTO scoreDTO;
+	
+	private MovieEntity movie;
+	
+	private UserEntity user;
+	
+	@Mock
+	private CustomUserUtil userUtil;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingMovieId = 1L;
+		nonExistingMovieId = 1000L;
+		
+		user = UserFactory.createUserEntity();
+		
+		score = ScoreFactory.createScoreEntity();
+		scoreDTO = ScoreFactory.createScoreDTO();
+		
+		movie = MovieFactory.createMovieEntity();
+		movie.getScores().add(score);
+		movie.setCount(movie.getScores().size());
+		
+		Mockito.when(userService.authenticated()).thenReturn(user);
+		
+		Mockito.when(movieRepository.findById(existingMovieId)).thenReturn(Optional.of(movie));
+		Mockito.when(movieRepository.findById(nonExistingMovieId)).thenReturn(Optional.empty());
+		
+		Mockito.when(scoreRepository.saveAndFlush(any())).thenReturn(score);
+		Mockito.when(movieRepository.save(any())).thenReturn(movie);
+	}
+	
 	@Test
 	public void saveScoreShouldReturnMovieDTO() {
+		
+		MovieDTO result = service.saveScore(scoreDTO);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getScore(), 4.5);
+		Assertions.assertEquals(result.getCount(), 1);
 	}
 	
 	@Test
 	public void saveScoreShouldThrowResourceNotFoundExceptionWhenNonExistingMovieId() {
+		ScoreDTO dto = new ScoreDTO(nonExistingMovieId, 4.5);
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.saveScore(dto);
+		});
 	}
 }
--- Desafio DSMovie Jacoco/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ Desafio DSMovie Jacoco/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,10 +1,17 @@
 package com.devsuperior.dsmovie.services;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.projections.UserDetailsProjection;
+import com.devsuperior.dsmovie.repositories.UserRepository;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
@@ -12,7 +19,7 @@ public class UserServiceTests {
 
 	@InjectMocks
 	private UserService service;
-
+	
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
 	}
